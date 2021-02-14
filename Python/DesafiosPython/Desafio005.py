5645534c68003fd34e5697cbbe29bb1f0fdb5a58
Guilherme Simao <guilhermesimaogh@outlook.com>
2021-02-14 19:35:15 -0300

    Delete Python directory

--- Python/DesafiosPython/Desafio005.py
+++
@@ -1,6 +0,0 @@
-#Faca um programa que leia um numero inteiro e mostre na tela seu sucessor e antecessor
-print('{:=^50}'.format('Sucessor e Antecessor'))
-n = int(input('Escreva um numero : '))
-sucessor = n+1
-antecessor = n-1
-print('Sucessor : {}{}{}\t\t\tAntecessor : {}{}{}'.format('\033[32m',sucessor,'\033[m','\033[36m',antecessor,'\033[m'))
