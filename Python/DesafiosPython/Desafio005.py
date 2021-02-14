f623c6d6e6eee28046355ddd65ba721eec5450d1
GuilhermeSimaos <xpwp0312@live.com>
2021-02-13 23:27:44 -0300

    Sucessor e Antecessor

--- Python/DesafiosPython/Desafio005.py
+++ Python/DesafiosPython/Desafio005.py
@@ -1,6 +1,6 @@
-# faca um programa que leia um numero inteiro e mostre na tela seu sucessor e antecessor
+'''Faca um programa que leia um numero inteiro e mostre na tela seu sucessor e antecessor'''
+print('{:=^50}'.format('Sucessor e Antecessor'))
 n = int(input('Escreva um numero : '))
 sucessor = n+1
 antecessor = n-1
-print('{:=^20}'.format('Resultados'))
-print('Sucessor : {}\nAntecessor : {}'.format(sucessor,antecessor))
+print('Sucessor : {}{}{}\t\t\tAntecessor : {}{}{}'.format('\033[32m',sucessor,'\033[m','\033[36m',antecessor,'\033[m'))
