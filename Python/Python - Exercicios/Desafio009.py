2cde67e878a79c6374828160a69bd642c1c1fec9
GuilhermeSimaos <xpwp0312@live.com>
2021-02-14 19:44:29 -0300

    Tabuada de um numero

---
+++ Python/Python - Exercicios/Desafio009.py
@@ -0,0 +1,7 @@
+#Faca um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada
+n = int(input('Digite um numero inteiro : '))
+print('\n{:=^30}'.format('Tabuada'))
+print('{}x1 = {}\n{}x2 = {}\n{}x3 = {}'.format(n,(n*1),n,(n*2),n,(n*3)))
+print('{}x4 = {}\n{}x5 = {}\n{}x6 = {}'.format(n,(n*4),n,(n*5),n,(n*6)))
+print('{}x7 = {}\n{}x8 = {}\n{}x9 = {}'.format(n,(n*7),n,(n*8),n,(n*9)))
+print('{}*10 = {}'.format(n,(n*10)))
