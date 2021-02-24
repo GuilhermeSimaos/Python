f36d537acee0678eace917f78985b70613374642
GuilhermeSimaos <xpwp0312@live.com>
2021-02-24 20:30:45 -0300

    Conversor de Medidas (V2)

--- Python/Python - Exercicios/Desafio008.py
+++ Python/Python - Exercicios/Desafio008.py
@@ -1,7 +1,13 @@
-#Escreva um programa que leia um valor em metros e exiba convertido em centimetros e
-#milimetros
-m = float(input('Digite o valor em metros: '))
-c = m*100
-mi = m*1000
-print('\n{:=^20}'.format('Resultado'))
-print('Metros = {}m\nCentimetros = {}cm\nMilimetros = {}mm\n'.format(m,c,mi))
+# Escreva um programa que leia um valor em metros e exiba convertido em centimetros e milimetros
+print('{}{:~^50}{}'.format('\033[35m','Conversor de medidas','\033[m'))
+metros = float(input('Digite um valor (em metros) para a conversao: '))
+centimentros = metros * 100
+milimetros = metros * 1000
+kilometros = metros / 1000 #bonus
+milhas = metros / 1609 #bonus
+jardas = metros * 1.094 #bonus
+pes = metros * 3.281 #bonus
+print('{}{:~^50}{}'.format('\033[35m','Resultados','\033[m'))
+print('Metros = {} m(s)\n\nCentimetros = {} cm(s)\nMilimetros = {} mm(s)'.format(metros,centimentros,milimetros))
+print('Kilometros = {} km(s)\n\nMilhas = {:.3f} mile(s)\nJardas = {:.3f} yd(s)\nPes = {:.3f} ft(s)'.format(kilometros,milhas,jardas,pes))
+print('{}{:~^50}{}'.format('\033[35m','','\033[m'))
