6964ce8e039ab9f1f1a49f340cbfce06704082a1
GuilhermeSimaos <xpwp0312@live.com>
2021-02-24 20:31:04 -0300

    Tabuada de um numero (V2)

--- Python/Python - Exercicios/Desafio009.py
+++ Python/Python - Exercicios/Desafio009.py
@@ -1,7 +1,11 @@
 #Faca um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada
-n = int(input('Digite um numero inteiro : '))
-print('\n{:=^30}'.format('Tabuada'))
-print('{}x1 = {}\n{}x2 = {}\n{}x3 = {}'.format(n,(n*1),n,(n*2),n,(n*3)))
-print('{}x4 = {}\n{}x5 = {}\n{}x6 = {}'.format(n,(n*4),n,(n*5),n,(n*6)))
-print('{}x7 = {}\n{}x8 = {}\n{}x9 = {}'.format(n,(n*7),n,(n*8),n,(n*9)))
-print('{}*10 = {}'.format(n,(n*10)))
+from time import sleep
+print('{}{:.^45}{}'.format('\033[34m','Tabuada Personalizada','\033[m'))
+numero = int(input('Escolha um numero qualquer: '))
+print('{}Gerando tabela...'.format('\033[36m'))
+sleep(2)
+print('{}{:.^45}{}'.format('\033[34m','Tabuada','\033[m'))
+print('{} x 1 = {}   \t{} x 2 = {}   \t{} x 3 = {}'.format(numero,(numero*1),numero,(numero*2),numero,(numero*3)))
+print('{} x 4 = {}   \t{} x 5 = {}   \t{} x 6 = {}'.format(numero,(numero*4),numero,(numero*5),numero,(numero*6)))
+print('{} x 7 = {}   \t{} x 8 = {}   \t{} x 9 = {}\n{} x 10 = {}'.format(numero,(numero*7),numero,(numero*8),numero,(numero*9),numero,(numero*10)))
+print('\033[34m...\033[m'*15)
