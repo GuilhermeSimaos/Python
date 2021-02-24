75e1fa9b2adb1c36a78fefe743d0842ef7561537
GuilhermeSimaos <xpwp0312@live.com>
2021-02-24 20:30:14 -0300

    Dobro, Triplo e Raiz (V2)

--- Python/Python - Exercicios/Desafio006.py
+++ Python/Python - Exercicios/Desafio006.py
@@ -1,7 +1,9 @@
-#crie um algoritmo que leia um numero e mostre seu dobro, triplo e raiz quadrada
-n = int(input('Digite um numero qualquer: '))
-d = n*2
-t = n*3
-r = n**2
-print('\n{:=^20}'.format('Resultados'))
-print('Dobro = {}\nTriplo = {}\nRaiz Quadrada = {}\n'.format(d,t,r))
+# Crie um algoritmo que leia um numero e mostre seu dobro, triplo e raiz quadrada
+print('{}{:=^30}{}'.format('\033[1;33m','Calculadora','\033[m'))
+numero = int(input('Digite um numero qualquer: '))
+dobro = numero*2
+triplo = numero*3
+raizQ = numero**2
+print('{}{:=^30}{}'.format('\033[1;33m','','\033[m'))
+print('Dobro = {}\nTriplo = {}\nRaiz Quadrada = {}'.format(dobro,triplo,raizQ))
+print('{}{:=^30}{}'.format('\033[1;33m','','\033[m\n'))
