ce5f6906db7550157fe938a911a34e61666399ef
GuilhermeSimaos <xpwp0312@live.com>
2021-02-24 20:30:29 -0300

    Media de um Aluno (V2)

--- Python/Python - Exercicios/Desafio007.py
+++ Python/Python - Exercicios/Desafio007.py
@@ -1,6 +1,8 @@
-#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a media
-n1 = int(input('Digite a primeira nota: '))
-n2 = int(input('Digite a segunda nota: '))
-m = (n1+n2)/2
-print('\n{:=^30}'.format('Resultado da nota'))
-print('Media do aluno = {}'.format(m))
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a media
+print('{}{:-^40}{}'.format('\033[32m','Media Escolar','\033[m'))
+nota1 = int(input('Qual foi a primeira nota? '))
+nota2 = int(input('Qual foi a segunda nota? '))
+media = (nota1 + nota2)/2
+print('{}{:-^40}{}'.format('\033[32m','Resultado da nota','\033[m'))
+print('Media do aluno = {}'.format(media))
+print('{}{:-^40}{}'.format('\033[32m','','\033[m\n'))
